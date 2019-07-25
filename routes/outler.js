9b8465824f0f2981c1d107785d3a5994ad91553e
Akash1507 <atakashsharma@gmail.com>
2019-07-26 03:00:26 +0530

    make updates to outler routes

--- routes/outler.js
+++ routes/outler.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
 
-const productController = require('../controllers/products')
+const outlerController = require('../controllers/outler')
 
 
 const router  = express.Router()
 
 
 
-router.get('/',productController.getProducts);
+router.get('/',outlerController.getIndex);
+router.get('/products', outlerController.getProducts);
+router.get('/cart', outlerController.getCart);
+router.get('/orders', outlerController.getOrders);
+router.get('/checkout', outlerController.getCheckout);
+
+
+
  
 
 module.exports = router;
