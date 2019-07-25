8f2191653d5d68cafa3a133517d8f1d1ae871617
Akash1507 <atakashsharma@gmail.com>
2019-07-25 20:38:59 +0530

    update file

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const bodyParser = require('body-parser');
 
 const path       = require('path');
 const rootDir    = require('./utils/path');
-const adminRoutes  = require('./routes/admin');
+const adminData  = require('./routes/admin');
 const outlerRoutes = require('./routes/outler'); 
 
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(outlerRoutes);
 
 app.use(express.static(path.join(rootDir,'public')));
 
 app.use('/',(req,res,next) =>{
-    res.status(404).sendFile(path.join(rootDir,"views","notfound.html"));
+    res.status(404).render('notfound',{pageTitle:"404 Page"});
 })
 
 
