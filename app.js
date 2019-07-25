8121d04478d013d2f97f001f38aa904be93ea1ed
Akash1507 <atakashsharma@gmail.com>
2019-07-25 08:13:52 +0530

    makes more clean the main file app.js

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const http = require('http');
+const express    = require('express');
+const bodyParser = require('body-parser');
 
-const express = require('express');
+const path       = require('path');
+const rootDir    = require('./utils/path');
+const adminRoutes  = require('./routes/admin');
+const outlerRoutes = require('./routes/outler'); 
 
 const app = express();
-const server = http.createServer(app);
 
-server.listen(3000);
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/admin',adminRoutes);
+app.use(outlerRoutes);
+
+app.use(express.static(path.join(rootDir,'public')));
+
+app.use('/',(req,res,next) =>{
+    res.status(404).sendFile(path.join(rootDir,"views","notfound.html"));
+})
+
+
+
+app.listen(3000);
