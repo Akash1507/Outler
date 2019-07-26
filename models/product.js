b02cffb9d60bb86cb90069f9eb38df8a5201c861
Akash1507 <atakashsharma@gmail.com>
2019-07-27 04:36:13 +0530

    cart function are added

--- models/product.js
+++ models/product.js
@@ -15,7 +15,8 @@ const getProductFromFile = cb => {
 const products = []
 
 module.exports = class Product{
-    constructor(title, imageUrl, description, price ){
+    constructor(id,title, imageUrl, description, price ){
+        this.id          = id;
         this.title       = title;
         this.imageUrl    = imageUrl;
         this.description = description;
@@ -23,15 +24,40 @@ module.exports = class Product{
         }
 
     save(){
+        
         getProductFromFile( products => {
+            if (this.id){
+            const existingProductIndex = products.findIndex(prod => prod.id ===this.id);
+            const updatedProduct = [...products];
+            updatedProduct[existingProductIndex] = this;
+            fs.writeFile(p,JSON.stringify(updatedProduct), (err) =>{
+                console.log(err);
+            });
+            }else{
+            this.id = Math.random().toString();
             products.push(this);
             fs.writeFile(p,JSON.stringify(products), (err) =>{
                 console.log(err);
             });
+        }
         })
     }
 
     static fetchAll(cb){
         getProductFromFile(cb);
+    }
+
+    static deleteById(id){
+        getProductFromFile(products =>{
+            const updateProducts = products.filter(prod => prod.id !== id);
+            cb(product);
+        })
+    }
+
+    static findById(id,cb) {
+        getProductFromFile(products =>{
+            const product = products.find(p => p.id === id);
+            cb(product);
+        })
     }
 }
